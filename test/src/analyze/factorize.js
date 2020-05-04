ef0810d4fc83332afd04c2711d181b017b93e191
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-05-04 22:07:44 +0200

    :boom: BREAKING CHANGE(factorize): Use generator.

--- test/src/analyze/factorize.js
+++ test/src/analyze/factorize.js
@@ -4,14 +4,23 @@ import { $2, iadd1, eq0, gt1, divmod } from "@aureooms/js-number" ;
 
 import * as prime from '../../../src' ;
 
-test ( "factorize" , t => {
+const factorize = prime . __factorize__ ( $2, iadd1, eq0, gt1, divmod ) ;
 
-	const factorize = prime . __factorize__ ( $2, iadd1, eq0, gt1, divmod ) ;
+function macro ( t , x , e ) {
+	const got = [ ...factorize ( x ) ] ;
+	t.deepEqual ( e , got ) ;
+}
 
-	const factors = [ ] ;
+macro.title = ( providedTitle , x , e ) => `factorize(${x}) = ${JSON.stringify(e)}` ;
 
-	factorize ( 15, factors ) ;
-
-	t.deepEqual ( factors , [ 3 , 5 ] , "15 is 3 * 5" ) ;
-
-} ) ;
+test( macro ,   1 , [ ] ) ;
+test( macro ,   2 , [ 2 ] ) ;
+test( macro ,   3 , [ 3 ] ) ;
+test( macro ,   4 , [ 2 , 2 ] ) ;
+test( macro ,   5 , [ 5 ] ) ;
+test( macro ,   6 , [ 2 , 3 ] ) ;
+test( macro ,   7 , [ 7 ] ) ;
+test( macro ,   8 , [ 2 , 2 , 2 ] ) ;
+test( macro ,  15 , [ 3 , 5 ] ) ;
+test( macro ,  17 , [ 17 ] ) ;
+test( macro , 999 , [ 3 , 3 , 3 , 37 ] ) ;
