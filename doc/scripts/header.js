fbc897b75be27a6a0032eb441aa1717349e53c56
a flying potato <80830782+a-flying-potato@users.noreply.github.com>
2022-10-29 16:24:37 +0200

    :robot: docs: Use codecov as test link.
    These changes were automatically generated by a transform whose code can be found at:
      - https://github.com/make-github-pseudonymous-again/rejuvenate/blob/e22c2a19ef974c2f709a2983f8eb026496ef1d10/src/transforms/docs:use-codecov-as-test-link.js
    Please contact the author of the transform if you believe there was an error.

--- doc/scripts/header.js
+++ doc/scripts/header.js
@@ -17,7 +17,7 @@ domReady(() => {
 	header.insertBefore(projectname, header.firstChild);
 
 	const testlink = document.querySelector('header > a[data-ice="testLink"]');
-	testlink.href = 'https://coveralls.io/github/number-theoretic/primes';
+	testlink.href = 'https://app.codecov.io/gh/number-theoretic/primes';
 	testlink.target = '_BLANK';
 
 	const searchBox = document.querySelector('.search-box');
